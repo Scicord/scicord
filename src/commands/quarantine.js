d70a61d9ab6b35db0dee264234260b6314a0ee00
Free Memory <michael.acobas@baschel.io>
2021-07-14 23:37:13 +0300

    relax the channel creation overwrites

--- src/commands/quarantine.js
+++ src/commands/quarantine.js
@@ -8,7 +8,7 @@ module.exports = class Quarantine extends Command
     }
 
     userPermissionsToExecute = () => {
-        return ['BAN_MEMBERS'];
+        return ['MANAGE_ROLES', 'MANAGE_CHANNELS'];
     }
 
     execute = (botClient, message) => {
@@ -16,28 +16,32 @@ module.exports = class Quarantine extends Command
         if(!toQuarantine)
             return;
         
-        const maxQtChannel = message.guild.channels.cache.filter(channel => channel.name.startsWith('quarantine-'))
+        const guild = message.guild;      
+        const everyoneRole = guild.roles.everyone;          
+        const maxQtChannel = guild.channels.cache.filter(channel => channel.name.startsWith('quarantine-'))
             .map(channel => parseInt(channel.name.split('-')[1], 10)).sort().pop() || 0;
-    
-        const modRoleOverrides = message.guild.roles.cache.filter(role => role.permissions.has('BAN_MEMBERS')).map(role => ({
+                
+
+        const modRoleOverrides = guild.roles.cache.filter(role => role.permissions.has('MANAGE_ROLES')).map(role => ({
             type: 'role',
             id: role.id,
-            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY', 'MANAGE_MESSAGES']
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES']
         }));        
     
         // Allow myself.
         modRoleOverrides.push({
             type: 'member',
             id: message.guild.me.id,
-            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES', 'READ_MESSAGE_HISTORY', 'MANAGE_MESSAGES']
+            allow: ['VIEW_CHANNEL', 'SEND_MESSAGES']
         });
     
-        message.guild.channels.create(`quarantine-${maxQtChannel + 1}`, {
+        guild.channels.create(`quarantine-${maxQtChannel + 1}`, {
             reason: `Quarantine for user ${toQuarantine.displayName} requested by ${message.member.displayName}`,
+            type: 'text',
             permissionOverwrites: [{
                 type: 'role',
                 deny: 'VIEW_CHANNEL',
-                id: message.guild.id,            
+                id: everyoneRole.id
             }, {
                 type: 'member',
                 allow: ['VIEW_CHANNEL', 'SEND_MESSAGES'],
@@ -45,6 +49,6 @@ module.exports = class Quarantine extends Command
             }, ...modRoleOverrides]
         }).then(channel => {
             channel.send(`You have been quarantined, <@${toQuarantine.id}>. Attempting to re-rank as Default or leave the server will result in an instant ban. A mod will join presently.` );
-        }); 
+        }).catch(console.error);
     }
 };
