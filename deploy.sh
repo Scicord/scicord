205b403c04a327bd07ef5956ced4eeef8d7231b0
grromfy <kevin@pinger.com>
2021-09-13 10:26:29 -0700

    Quick fire and test

--- deploy.sh
+++ deploy.sh
@@ -14,12 +14,6 @@ if [[ $(git status -s) ]]; then
   shouldInstallNewCode=false
 fi
 
-currentBranch=$(git rev-parse --abbrev-ref HEAD)
-if [[ "$currentBranch" != "master" ]]; then
-  echo 'Skipping code update, not on master.'
-  shouldInstallNewCode=false
-fi
-
 unpushedCommits=$(git log origin/master..master)
 if [[ $unpushedCommits ]]; then
   echo "Skipping code update, master has unpushed commits"
