57a56db300b435ba7d488311ca68602993dc22c1
grromfy <kevin@pinger.com>
2021-09-30 15:33:02 -0700

    Initial commit for >stuckchannel

---
+++ src/commands/stuckchannel.js
@@ -0,0 +1,57 @@
+"use strict";
+const Command = require('./command');
+const Config = require('../../config/command/quarantine.json')
+const { MessageEmbed } = require('discord.js');
+const CommandConfig = require('../config/commandConfig');
+const log = require('../utils/logger')();
+
+module.exports = class Stuckchannel extends Command
+{
+    constructor()
+    {
+        super();
+        this.config = new CommandConfig(Config);
+    }
+
+    commandConfig = () => {
+        return this.config;
+    }
+
+    usage = () => {
+        return new MessageEmbed()
+            .setTitle("Stuck Channel")
+            .setDescription(`Usage: \`>stuckchannel\``)
+            .setFooter("Removes any channel with quarantine prefix");
+    }
+
+    execute = (botClient, message) => {
+        const args = this.args(botClient, message);
+        const guild = message.guild;
+
+        if(args[0] === "help") {
+            message.channel.send({
+                embed: this.usage()
+            });
+            return;
+        }
+
+        const {channelPrefix} = this.commandConfig().getConfig();
+        const transientChannels = botClient.transientChannels()
+
+        const stuckChannels = guild.channels.cache.array().filter(channel => channel.name.includes(channelPrefix));
+        stuckChannels.forEach(channel => {
+            log.info(`Deleting #${channel.name} (${channel.id})`);
+            transientChannels.destroyChannel(channel.id);
+            channel.delete();
+        })
+
+        botClient.auditLog({
+            embed: new MessageEmbed()
+                .setTitle('Stuck Channel')
+                .setDescription(`Channels with prefix \`${channelPrefix}\` were removed!`)
+                .addField("Moderator", message.author, false)
+        });
+
+        log.info(`Successfully removed any stuck channels!`);
+    }
+};
